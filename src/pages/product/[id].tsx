2b5ef9fc4c6d4915f833b6773c86510bc527bcc5
Marcos Eduardo <maarcosefb0@gmail.com>
2023-01-14 09:31:06 -0300

    feat: checkoutSession and payments

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -16,6 +16,7 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
@@ -24,6 +25,10 @@ export default function Product({ product }: ProductProps) {
 
   if (isFallback) {
     return <p>Loading...</p>;
+  }
+
+  function handleBuyProduct(){
+    console.log(product.defaultPriceId);
   }
 
   return (
@@ -37,7 +42,7 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
 
         <p>{product.description}</p>
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -72,6 +77,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: "BRL",
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, //1 hour
